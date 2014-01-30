3c7f9c744a7a45b082aa5199507b4f9e933c5921
RachelBailin <rachelebailin@gmail.com>
2014-01-30 15:08:45 -0800

    step 5

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,16 +3,16 @@ class Book
   field :title, type: String
   field :author, type: String
   field :year, type: Integer
-  field :ISBN, type: Interger
+  field :ISBN, type: Integer
   field :genre, type: String
 
 
 
 validates :title, presence: true, uniqueness: true
-validates :author, presence: true, 
-validates :year, presence: true, 
+validates :author, presence: true
+validates :year, presence: true
 validates :ISBN, presence: true, uniqueness: true
-validates :genre, presence: true,
+validates :genre, presence: true
 
 
 end
